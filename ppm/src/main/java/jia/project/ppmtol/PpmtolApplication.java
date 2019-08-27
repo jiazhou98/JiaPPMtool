a2ec1c8a5d943e9048d444bfa70a36249d83345e
Jia Zhou <jiaz2@DESKTOP-FK7C9MM.localdomain>
2019-08-27 01:41:46 -0500

    initial commit

---
+++ ppm/src/main/java/jia/project/ppmtol/PpmtolApplication.java
@@ -0,0 +1,13 @@
+package jia.project.ppmtol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpmtolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PpmtolApplication.class, args);
+    }
+
+}
